03e28969e2bcf608389ca9cd31a63c309c684b92
Derek Enos <derekenos@archive.org>
2026-03-18 16:39:56 +0000

    Revert "Merge branch 'derekenos/cdx-dataset-preview' into 'main'"
    This reverts merge request !74

--- keystone/views.py
+++ keystone/views.py
@@ -479,8 +479,6 @@ def dataset_detail(request, dataset_id):
             "disable_publishing": not user.has_perm(
                 Permissions.PUBLISH_DATASET, dataset
             ),
-            "output_is_cdx": dataset.job_start.job_type_id
-            == settings.KnownArchJobUuids.CDX_DATASET,
         },
     )
 
